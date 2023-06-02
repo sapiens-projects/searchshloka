bc0febebe16026a598d4337fd4784f058ff80baf
sapien-projects <131551647+sapien-projects@users.noreply.github.com>
2023-06-02 22:19:19 +0530

    Add files via upload

--- page_layers/shloka_search_div/shloka_search_div.js
+++ page_layers/shloka_search_div/shloka_search_div.js
@@ -1,23 +1,23 @@
 'use strict'
-
+ 
 
 function addlyrShlokaSearchDiv(attachTo, thisFileLoc='/searchshloka/page_layers/shloka_search_div/'){
 	var bSites = null;
 
 	var code = `
 		<div id='shlokaSearchDiv'>
-			<select class="searchInputBox">
+			<select class="inputBox">
 				<option name="prof1" value="prof1">Prof1</option>
 				<option name="prof2" value="prof2">Prof2</option>
 				<option name="prof3" value="prof3">Prof3</option>
 			</select>
-			<select class="searchInputBox">
+			<select class="inputBox">
 				<option name="prof1" value="astang">Astang</option>
 				<option name="rachnaSharir" value="rachnaSharir">Rachna Sharir</option>
 				<option name="kriyaSharir" value="kriyaSharir">Kriya Sharir</option>
 			</select>
-			<input class="searchInputBox" maxlength="4" name="pageNum" type="number" placeholder="page number">
-			<input class="button" value="search">
+			<input class="inputBox" maxlength="4" min='1' max="9999" name="pageNum" type="number" placeholder="page number">
+			<input type="button"  class="inputBox button" value="search">
 			<link rel="stylesheet" href="${thisFileLoc}main.css">
 		</div>
 	`;
--- page_layers/site_header_footer/site_header_footer.js
+++ page_layers/site_header_footer/site_header_footer.js
@@ -4,25 +4,27 @@
 function addlyrSiteHeaderFooter(attachTo, thisFileLoc='/search_shloka/page_layers/site_header_footer/'){
 	const bsites = ['pageMid']
 	const code = `
-		<a href="#" id="siteNameBannerAnchor">
-		<div id="siteNameBanner">
-		<img src="${thisFileLoc}images/logo.png" alt="logo">
-		<p>search shloka</p>
+		<div id="rootLayer">
+			<a href="#" id="siteNameBannerAnchor">
+			<div id="siteNameBanner">
+			<img src="${thisFileLoc}images/logo.png" alt="logo">
+			<p>search shloka</p>
+			</div>
+			</a>
+			
+			<div id="${bsites[0]}"></div>
+			
+			<div id="siteFooterBanner">
+				<p class="footerHeading">Quick Links</p>
+				<ul class="quickLinks">
+					<li><a href="./mislleneous/shlokas/001.html">Todays Shloka</a></li>
+					<li><a href="/hello.html">hello page</a></li>
+				</ul>
+				<p class="siteDevCredit">Build and maintain with <img src="${thisFileLoc}images/heart.jpg" alt="love"> by BAMS 1st Year students of session '22-23' of SPIAMSR.</p>
+			</div>
+			
+			<link rel="stylesheet" href="${thisFileLoc}main.css">
 		</div>
-		</a>
-		
-		<div id="${bsites[0]}"></div>
-		
-		<div id="siteFooterBanner">
-			<p class="footerHeading">Quick Links</p>
-			<ul class="quickLinks">
-				<li><a href="./mislleneous/shlokas/001.html">Todays Shloka</a></li>
-				<li><a href="/hello.html">hello page</a></li>
-			</ul>
-			<p class="siteDevCredit">Build and maintain with <img src="${thisFileLoc}images/heart.jpg" alt="love"> by BAMS 1st Year students of session '22-23' of SPIAMSR.</p>
-		</div>
-		
-		<link rel="stylesheet" href="${thisFileLoc}main.css">
 		`;
 
 	document.getElementById(attachTo[0]).innerHTML += code
